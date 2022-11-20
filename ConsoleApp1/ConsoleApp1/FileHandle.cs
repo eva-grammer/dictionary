97aea0e6620a51954fa8f7802e586c7e8d8641ab
Yonglie Lu <YLu27@slb.com>
2022-11-20 17:58:05 +0800

    add

---
+++ ConsoleApp1/ConsoleApp1/FileHandle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class FileHandle
+    {
+        
+        const string regStr = @"\*\*[A-Za-z]+\*\*\x20*\\\[.+?\]";
+        const string regStr1 = @"\*\*[A-Za-z]+\*\*\x20";
+         internal static void ReplaceOneFile(string fileName)
+        {
+            var content = System.IO.File.ReadAllText(fileName);
+
+
+            var matchs = Regex.Matches(content, @regStr);
+            foreach (Match item in matchs)
+            {
+                var match = Regex.Match(item.Value, regStr1);
+                var word = match.Value.Trim('*', ' ');
+                var link = $"[英音](https://dict.youdao.com/dictvoice?audio={word}\\&type=1)  [美音](https://dict.youdao.com/dictvoice?audio={word}\\&type=2) ";
+                content = content.Replace(item.Value, $"{item.Value} {link}");
+            }
+
+            System.IO.File.WriteAllText(fileName, content);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            var path = @"C:\BaiduNetdiskDownload\markdown\github\dictionary\docs1";
+          var files=  System.IO.Directory.GetFiles(path);
+            foreach (var item in files)
+            {
+                var file = new FileInfo(item);
+                var name = file.Name;
+                if (name.StartsWith("第") && name.EndsWith(".md"))
+                {
+
+
+                 FileHandle.   ReplaceOneFile(item);
+
+                }
+            }
+            Console.WriteLine("finished...");
+            Console.ReadLine();
+        }
+    }
+}
